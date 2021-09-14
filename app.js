83be56041aef4db61a66708e0be834585b285332
Dickson <dickson.njuguna@student.moringaschool.com>
2021-09-14 12:11:03 +0300

    all changes done

--- app.js
+++ app.js
@@ -1,10 +1,93 @@
+  $(document).ready(function () {
+    $("#dd1").click(function () {
+      $("#dd1").hide();
+      $("#dd2").show();
+      $("#dd2").click(function () {
+        $("#dd2").hide();
+        $("#dd1").show();
+      });
+    });
+    $("#dev1").click(function () {
+      $("#dev1").hide();
+      $("#dev2").show();
+      $("#dev2").click(function () {
+        $("#dev2").hide();
+        $("#dev1").show();
+      });
+    });
   
-
-
-
-
-
-
+    $("#pm1").click(function () {
+      $("#pm1").hide();
+      $("#pm2").show();
+      $("#pm2").click(function () {
+        $("#pm2").hide();
+        $("#pm1").show();
+      });
+    });
+    $("#black").hover(function () {
+      $("#BS").show();
+    },
+    function(){
+      $("#BS").hide();
+    });
+  
+    $("#ontariosecurity").hover(function () {
+      $("#sec").show();
+    },
+    function(){
+        $("#sec").hide();
+    })
+  
+    $("#Pyramids").hover(function () {
+      $("#pyr").show();
+    },
+    function(){
+        $("#pyr").hide();
+    })
+  
+  
+    $("#JimCarry").hover(function () {
+      $("#jim").show();
+    },
+    function(){
+        $("#jim").hide();
+    })
+  
+    $("#NiceDay").hover(function () {
+      $("#Nice").show();
+    },
+    function(){
+        $("#Nice").hide();
+    })
+  
+    $("#Calculator").hover(function () {
+      $("#calc").show();
+    },
+    function(){
+        $("#calc").hide();
+    })
+  
+    $("#burned").hover(function () {
+      $("#Burn").show();
+    },
+    function(){
+        $("#Burn").hide();
+    })
+  
+    $("#Giraffe").hover(function () {
+      $("#Giraffe").show();
+    },
+    function(){
+        $("#GR").hide();
+    })
+  
+  $("#submit").click(function (e) {
+      e.preventDefault();
+      
+      alert("message recived.thanks")
+  })
+  
+  });
 
 
 
